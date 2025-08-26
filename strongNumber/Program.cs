d4f5cb88803d68cb4bbb924acdcb16f583d3ed81
Mert <turkmen491@hotmail.com>
2025-08-26 22:03:17 +0300

    Strong Number algoritma sorusu eklendi

---
+++ strongNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿
+    /*
+    1)Başla
+    2)Kullanıcıdan bir sayı al
+    3)Sayıyı geçici bir değişkene ata
+    4)Toplamı 0 olarak başlat
+    5)Geçici değişken sıfırdan büyük olduğu sürece:
+        a)Geçici değişkenin son basamağını al
+        b)Bu basamağın faktöriyelini hesapla
+        c)Faktöriyeli toplama ekle
+        d)Geçici değişkeni 10'a böl (son basamağı at)
+    6)Eğer toplam, orijinal sayıya eşitse:
+        a)Sayı güçlü sayıdır    
+    7)Değilse:
+        a)Sayı güçlü sayı değildir
+    8)Bitir
+    */
+
+        Console.WriteLine("Bir sayı giriniz:");
+        int number = int.Parse(Console.ReadLine());
+
+        int temp = number;
+        int sum = 0;
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+
+            int faktorial = 1;
+            for (int i = 1; i <= digit; i++)
+            {
+                faktorial *= i;
+            }
+            sum += faktorial;
+            temp /= 10;
+        }
+
+        if (sum == number)
+        {
+            Console.WriteLine($"{number} sayısı bir güçlü sayıdır.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} sayısı bir güçlü sayı değildir.");
+        }
+    
